120f6d03a6c2e99d5c642660b5118b4f139ee1e0
AnanaSsSsSsS <elias.gold@bk.ru>
2023-01-12 20:29:20 +0300

    something looks like edit view added

--- Kopilka/View/EditView.swift
+++ Kopilka/View/EditView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct EditView: View {
+    
+    @EnvironmentObject var kopilkaViewModel: KopilkaViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        List {
+            ForEach(kopilkaViewModel.targets) { eachTarget in
+  
+                KopilkaViewItem2(target: eachTarget)
+
+            }
+            .onMove(perform: kopilkaViewModel.moveTargets)
+            .onDelete(perform: kopilkaViewModel.deleteTargets)
+        }
+        .listStyle(PlainListStyle())
+        
     }
 }
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
         EditView()
+            .environmentObject(KopilkaViewModel())
     }
+}
+
+
+struct KopilkaViewItem2 : View {
+    
+    let target: KopilkaModel
+    
+    var body: some View {
+        
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.right")
+            }
+            .frame(width: 40, height: 40)
+
+            
+            Text(target.title)
+                .lineLimit(1)
+                .foregroundColor(.red)
+                .padding(.leading, 9)
+            
+            Spacer()
+            
+            Image(systemName: "list.dash")
+        }
+        
+    }
+    
 }
